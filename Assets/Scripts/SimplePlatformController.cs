cfae2a739654381553f0b69e158d46779dfc0121
Michael Bandola <24bandola@gmail.com>
2017-06-06 19:54:28 -0400

    scene1

--- Assets/Scripts/SimplePlatformController.cs
+++ Assets/Scripts/SimplePlatformController.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
-
-	[HideInInspector] public bool facingRight = true;
+	[HideInInspector] public bool facingRight=true;
 	[HideInInspector] public bool jump=true;
-	 
-	public float moveForce = 365f;
+
+	public float moveForce=365f;
 	public float maxSpeed = 5f;
-	public float jumpForce = 1000f;
+	public float jumpForce=1000f;
 	public Transform groundCheck;
 
-	private bool grounded = false;
+	private bool grounded =false;
 	private Animator anim;
 	private Rigidbody2D rb2d;
 	// Use this for initialization
 	void Awake () {
-		anim = GetComponent<Animator> ();
+		anim = GetComponent<Animator>();
 		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
@@ -28,29 +27,29 @@ public class NewBehaviourScript : MonoBehaviour {
 			jump = true;
 		}
 	}
-
-	void FixedUpdate() {
+	void FixedUpdate()
+	{
 		float h = Input.GetAxis ("Horizontal");
 		anim.SetFloat ("Speed", Mathf.Abs (h));
-		if (h*rb2d.velocity.x < maxSpeed)
+		if (h * rb2d.velocity.x < maxSpeed)
 			rb2d.AddForce (Vector2.right * h * moveForce);
 		if (Mathf.Abs (rb2d.velocity.x) > maxSpeed)
-		rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+			rb2d.velocity = new Vector2 (Mathf.Sign (rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
 		if (h > 0 && !facingRight)
 			Flip ();
 		else if (h < 0 && facingRight)
 			Flip ();
-		if (jump)
-		{
+		if (jump) {
 			anim.SetTrigger ("Jump");
 			rb2d.AddForce (new Vector2 (0f, jumpForce));
-				jump = false;
-				}
-				}
-	void Flip(){
+			jump = false;
+		}
+	}
+	void Flip()
+	{
 		facingRight = !facingRight;
 		Vector3 theScale = transform.localScale;
-		theScale.x = -1;
+		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
 }
